afa0faa271473afc35a83d277bac95d76f856668
RenatoFrancisco <renatofrancisco.s@gmail.com>
2019-05-12 12:14:37 -0300

    4.2 O operador pipeline.

---
+++ PrimeiraAplicacao.Funcional/4_Operadores.Funcional/OperadorPipeline.fs
@@ -0,0 +1,24 @@
+﻿module OperadorPipeline
+
+let multiplicaPor2 numero = numero * 2
+
+let dobrarValoresDeUmaListaComOperador() =
+    [0..10]
+    |> List.filter (fun numero -> numero < 5)
+    |> List.map multiplicaPor2
+
+let retorno = dobrarValoresDeUmaListaComOperador()
+
+let somarCom10 numero = numero + 10
+let somarCom2 numero = numero + 2
+
+let variasOperacoes valor1 valor2 =
+    valor1 + valor2
+    |> somarCom10
+    |> somarCom2
+    |> multiplicaPor2
+
+let retorno1 = variasOperacoes 2 3
+
+let exemploInverso valor1 valor2 =
+    somarCom10 <| valor1 + valor2

9b2eb0bc99d920204b4547401d2e75377cdf3e3a
renatofrancisco <renatofrancisco.s@gmail.com>
2019-11-10 23:07:11 -0300

    5.2 Controle de Fluxo com Expressões

---
+++ PrimeiraAplicacao.Funcional/Tipos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int Fatorial(int numero)
+        {
+            int acumulador = numero;
+
+            for (int n = numero - 1; n >= 0; n--)
+                acumulador *= numero;
+
+            return acumulador;
+        }
+    }
+}

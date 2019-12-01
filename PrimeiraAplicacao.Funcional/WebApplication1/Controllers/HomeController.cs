1d5c8c625078230caa0d5380e0ca2fc5e5f4a7e6
renatofrancisco <renatofrancisco.s@gmail.com>
2019-12-01 18:40:29 -0300

    6.1 Paginas e Serviços

--- PrimeiraAplicacao.Funcional/WebApplication1/Controllers/HomeController.cs
+++ PrimeiraAplicacao.Funcional/WebApplication1/Controllers/HomeController.cs
@@ -28,6 +28,13 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult OlaMundo()
+        {
+            ViewBag.Title = "Olá mundo";
+
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

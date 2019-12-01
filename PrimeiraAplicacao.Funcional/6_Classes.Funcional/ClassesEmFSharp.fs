e0cd616c41feb2689fd33e7ed8612c0fd2ed8691
renatofrancisco <renatofrancisco.s@gmail.com>
2019-12-01 19:29:53 -0300

    6.2 Orientação a Objetos em F#

---
+++ PrimeiraAplicacao.Funcional/6_Classes.Funcional/ClassesEmFSharp.fs
@@ -0,0 +1,54 @@
+﻿module ClassesEmFSharp
+
+type Pessoa (nome: string, sobrenome: string) =
+    member this.Nome = nome
+    member this.Sobrenome = sobrenome
+
+let renato = Pessoa("Renato", "Francisco")
+//renato.Sobrenome <- "Silva"
+
+//type PessoaComGetESet (nome: string, sobrenome: string) = 
+//    let mutable _nome = nome
+//    let mutable _sobrenome = sobrenome
+
+//    member this.Nome
+//        with get() = _nome
+//        and set value = _nome <- sobrenome
+
+//    member this.Sobrenome
+//        with get() = _sobrenome
+//        and set value = _sobrenome <- sobrenome
+
+//let renato2 = PessoaComGetESet("Renato", "Francisco")
+
+type PessoaComAutoProperty (nome: string, sobrenome: string) =
+    member val Nome = nome with get, set
+    member val Sobrenome = sobrenome with get, set
+
+let renato3 = PessoaComAutoProperty("Renato", "Francisco")
+//renato.Sobrenome <- "Silva"
+
+type PessoaComIdade (idade: int) =
+    let mutable _maioridade = false
+    do
+        _maioridade <- idade >= 18
+
+    member val idade = idade with get, set
+    member val Maioridade = _maioridade with get
+
+type PessoaComHeranca (nome: string, sobrenome: string, idade: int) =
+    inherit PessoaComAutoProperty(nome, sobrenome)
+    let mutable _maioridade = false
+    do
+        _maioridade <- idade >= 18
+
+    member val idade = idade with get, set
+    member val maioridade = _maioridade with get
+
+let gabriel = PessoaComHeranca("Gabriel", "Schade", 26)
+
+
+
+
+
+
---
+++ PrimeiraAplicacao.Funcional/6_Classes.Funcional/InterfacesEmFSharp.fs
@@ -0,0 +1,18 @@
+﻿module InterfacesEmFSharp
+
+type IPessoa =
+    abstract member Nome: string
+    abstract member Sobrenome: string
+    abstract member NomeCompleto: string
+
+type PessoaComInterface (nome: string, sobrenome: string) =
+    member val Nome = nome with get, set
+    member val Sobrenome = sobrenome with get, set
+    
+    interface IPessoa with
+        member this.Nome = nome
+        member this.Sobrenome = sobrenome
+        member this.NomeCompleto = sprintf "%s %s" nome sobrenome
+
+
+

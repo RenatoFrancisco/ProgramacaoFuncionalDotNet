19e34805432d01af42e3f4b8167f32c40e8430b3
renatofrancisco <renatofrancisco.s@gmail.com>
2019-12-10 23:00:25 -0300

    7.3 Trabalhando com Lista Imutáveis

---
+++ PrimeiraAplicacao.Funcional/WebApplication.Funcional/Controllers/ClienteController.fs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication.Funcional.Controllers
+
+open Servicos
+open Microsoft.AspNetCore.Mvc
+
+[<ApiController>]
+[<Route("api/[controller]/[action]")>]
+type ClienteController() =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member __.ObterTodos() =
+        Persistencia.obterContexto().Clientes.Dados
+
+    [<HttpPost>]
+    member __.Incluir(cliente) = 
+        ClienteServico.incluirCliente cliente
+
+
--- PrimeiraAplicacao.Funcional/WebApplication.Funcional/Servicos.fs
+++ PrimeiraAplicacao.Funcional/WebApplication.Funcional/Servicos.fs
@@ -3,5 +3,23 @@
 open Dominio
 open Persistencia
 
-module ClienteServico = //...
+module ClienteServico =
+    let obterClientes() =
+        obterContexto().Clientes
+        
+    let incluirCliente cliente =
+        let tabela = obterClientes()
+        let dados = cliente :: tabela.Dados
+        salvarTabela { tabela with Dados = dados }
+
+    let excluirCliente id =
+        let tabela = obterClientes()
+        let dados = [
+            for cliente in tabela.Dados do
+                match cliente with
+                | cliente when cliente.Id = id -> ignore()
+                | cliente -> yield cliente
+        ]
+        salvarTabela { tabela with Dados = dados }
+
     
